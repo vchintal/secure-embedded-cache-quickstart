957b03c273ec291412594f042f043833837a5399
Vijay B. Chintalapati <Vijay.Chintalapati@gmail.com>
2014-05-27 22:05:48 -0700

    Initial revision
    Committing the first revision of files

---
+++ README.md
@@ -0,0 +1,9 @@
+Security in Embedded Cache
+========================
+
+This web application demonstrates how security in upcoming Infinispan versions works in 
+the embedded mode.
+
+Key things about the web application:
+------------
+1. The webapp is secured and one needs to login to access __ANY__ resource of the webapp
---
+++ src/main/java/org/infinispan/quickstart/securecache/rest/CacheEntry.java
@@ -0,0 +1,28 @@
+package org.infinispan.quickstart.securecache.rest;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonSerialize
+public class CacheEntry<K extends Comparable<K>,V extends Comparable<V>> implements Comparable<CacheEntry<K,V>> {
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	private K key; 
+	private V value; 
+	
+	public CacheEntry(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(CacheEntry<K, V> o) {
+		return this.key.compareTo(o.key);
+	}
+	
+}
